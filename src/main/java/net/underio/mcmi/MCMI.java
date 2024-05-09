81d0bc6324b1133b3eb5d66ba33bcf798b72de1a
Aaron Avdeev <underio3298@gmail.com>
2024-05-09 19:07:52 -0400

    Project setup

--- src/main/java/net/underio/mcmi/MCMI.java
+++ src/main/java/net/underio/mcmi/MCMI.java
@@ -6,13 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MCMI implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("mcmi");
+	public static final String MOD_ID = "mcmi";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info(MOD_ID + " has been initialized!");
 	}
 }
---
+++ src/main/java/net/underio/mcmi/MCMIClient.java
@@ -0,0 +1,10 @@
+package net.underio.mcmi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCMIClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
