43f7a0ca4a30ef3c33aa2a41438732e123371070
Aaron Avdeev <underio3298@gmail.com>
2024-05-10 18:20:42 -0400

    Change Block of Ruby Sound

--- src/main/java/net/underio/mcmi/block/ModBlocks.java
+++ src/main/java/net/underio/mcmi/block/ModBlocks.java
@@ -14,7 +14,7 @@ import net.underio.mcmi.MCMI;
 public class ModBlocks {
 
   public static final Block RUBY_BLOCK = registerBlock("ruby_block",
-      new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+      new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK)));
 
   public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
       new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
