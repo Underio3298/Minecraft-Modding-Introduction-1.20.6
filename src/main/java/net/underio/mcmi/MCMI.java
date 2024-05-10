3ee427bd4b6a36e4dbc1da853fada055c0cfd3e4
Aaron Avdeev <underio3298@gmail.com>
2024-05-09 20:25:20 -0400

    Add Ruby item
    Only in the Ingredients tab for now.
    Special Ruby tab will be implemented later.

--- src/main/java/net/underio/mcmi/MCMI.java
+++ src/main/java/net/underio/mcmi/MCMI.java
@@ -1,16 +1,18 @@
 package net.underio.mcmi;
 
 import net.fabricmc.api.ModInitializer;
+import net.underio.mcmi.item.ModItems;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MCMI implements ModInitializer {
-	public static final String MOD_ID = "mcmi";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+  public static final String MOD_ID = "mcmi";
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info(MOD_ID + " has been initialized!");
-	}
+  @Override
+  public void onInitialize() {
+    ModItems.RegisterModItems();
+    LOGGER.info(MOD_ID + " has been initialized!");
+  }
 }
---
+++ src/main/java/net/underio/mcmi/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.underio.mcmi.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.underio.mcmi.MCMI;
+
+public class ModItems {
+
+  public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+
+  private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+    entries.add(RUBY);
+  }
+
+  public static Item registerItem(String name, Item item) {
+    return Registry.register(Registries.ITEM, new Identifier(MCMI.MOD_ID, name), item);
+  }
+
+  public static void RegisterModItems() {
+    MCMI.LOGGER.info("Registering mod items for " + MCMI.MOD_ID);
+
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+  }
+}
